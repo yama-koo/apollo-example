520792b93a48f1cfb213cbbd9a015388bb89a112
yama-koo <yamako.ilc@gmail.com>
2019-05-19 03:16:51 +0900

    クライアント実装

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-// eslint-disable-next-line
-console.log('hello')
-const a = 1
+import { ApolloClient } from 'apollo-client'
+import { gql } from 'apollo-boost'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+// import fetch from 'node-fetch'
+import 'isomorphic-fetch'
+
+const link = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+  // fetch
+})
+
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+  link: link,
+  cache: cache
+})
+
+client
+  .query({
+    query: gql`
+      {
+        books {
+          title
+          author
+        }
+      }
+    `
+  })
+  .then(
+    (result): void => {
+      // eslint-disable-next-line no-console
+      console.log(JSON.stringify(result))
+    }
+  )
+  .catch(
+    (result): void => {
+      // eslint-disable-next-line no-console
+      console.error(result)
+    }
+  )
